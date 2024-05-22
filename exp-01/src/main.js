9ed314f89185d21f21c08532abcfee77662fd45c
hanlelin <han_le_lin@whut.edu.cn>
2024-05-22 17:57:45 +0800

    third

--- exp-01/src/main.js
+++ exp-01/src/main.js
@@ -4,11 +4,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import AMapLoader from '@amap/amap-jsapi-loader'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+AMapLoader.load({
+  key: '2a1c9ab319ba9bc0d4bd9dc882b7cf85',
+  version: '2.0',
+  plugins: ['AMap.Geocoder']
+}).then((AMap) => {
+  Vue.prototype.$AMap = AMap;
+
+  new Vue({
+    router,
+    store,
+    render: function (h) { return h(App) }
+  }).$mount('#app')
+
+}).catch(e => {
+  console.error(e);
+});
--- exp-01/src/views/userManage.vue
+++ exp-01/src/views/userManage.vue
@@ -125,7 +125,7 @@
           </el-date-picker>
         </el-form-item>
         <el-form-item label="省份">
-          <el-select v-model="form.province" class="full-width" placeholder="请选择">
+          <el-select v-model="form.province" class="full-width" placeholder="请选择" @change="loadCities">
           <el-option
             v-for="item in provinces"
             :key="item.value"
@@ -159,217 +159,213 @@
   </div>
 </template>
 
-            
-  <script>
-  export default {
-    name: 'MainPage',
-    data() {
-      return {
-        provinces: [
-        { value: 'beijing', label: '北京市' },
-        { value: 'tianjin', label: '天津市' },
-        { value: 'hebei', label: '河北省' },
-        { value: 'shanxi', label: '山西省' },
-        { value: 'neimenggu', label: '内蒙古自治区' },
-        { value: 'liaoning', label: '辽宁省' },
-        { value: 'jilin', label: '吉林省' },
-        { value: 'heilongjiang', label: '黑龙江省' },
-        { value: 'shanghai', label: '上海市' },
-        { value: 'jiangsu', label: '江苏省' },
-        { value: 'zhejiang', label: '浙江省' },
-        { value: 'anhui', label: '安徽省' },
-        { value: 'fujian', label: '福建省' },
-        { value: 'jiangxi', label: '江西省' },
-        { value: 'shandong', label: '山东省' },
-        { value: 'henan', label: '河南省' },
-        { value: 'hubei', label: '湖北省' },
-        { value: 'hunan', label: '湖南省' },
-        { value: 'guangdong', label: '广东省' },
-        { value: 'guangxi', label: '广西壮族自治区' },
-        { value: 'hainan', label: '海南省' },
-        { value: 'chongqing', label: '重庆市' },
-        { value: 'sichuan', label: '四川省' },
-        { value: 'guizhou', label: '贵州省' },
-        { value: 'yunnan', label: '云南省' },
-        { value: 'xizang', label: '西藏自治区' },
-        { value: 'shanxi2', label: '陕西省' },
-        { value: 'gansu', label: '甘肃省' },
-        { value: 'qinghai', label: '青海省' },
-        { value: 'ningxia', label: '宁夏回族自治区' },
-        { value: 'xinjiang', label: '新疆维吾尔自治区' },
-        { value: 'taiwan', label: '台湾省' },
-        { value: 'xianggang', label: '香港特别行政区' },
-        { value: 'aomen', label: '澳门特别行政区' }
-      ],
-        form: {
-          name: '',
-          date: '',
-          province: '',
-          city: '',
-          address: '',
-          zip: ''
-        },
-        inputUserName: '',
-        tableData: [{
-          date: '2016-05-02',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1517 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1519 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1516 弄',
-          zip: 200333
-        }],
-        headerColor: '#1E88E5',
-        mainColor: '#BBDEFB',
-        isadduser: false,
-      }
-
+<script>
+export default {
+  name: 'MainPage',
+  data() {
+    return {
+      provinces: [],
+      cities: [],
+      form: {
+        name: '',
+        date: '',
+        province: '',
+        city: '',
+        address: '',
+        zip: ''
+      },
+      inputUserName: '',
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1517 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1519 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1516 弄',
+        zip: 200333
+      }],
+      headerColor: '#1E88E5',
+      mainColor: '#BBDEFB',
+      isadduser: false,
+    };
+  },
+  mounted() {
+    this.loadProvinces();
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
     },
-    methods:{
-      handleOpen(key, keyPath) {
-        console.log(key, keyPath);
-      },
-      handleClose(key, keyPath) {
-        console.log(key, keyPath);
-      },
-      handleClick(row) {
-        console.log(row);
-      },
-      pushHome(){
-        this.$router.push('/home');
-      },
-      docManage(){
-        this.$router.push('/docmanage');
-      },
-      yellowPanel(){
-        this.headerColor='#b2b528'
-        this.mainColor='#fff957'
-      },
-      bluePanel(){
-        this.headerColor='#1E88E5'
-        this.mainColor='#BBDEFB'
-      },
-      adduser(){
-        console.log('adduser');
-        this.isadduser = true;
-      },
-      searchuser(){
-        console.log('searchuser');
-      },
-      quitForm(){
-        this.form = {
-          name: '',
-          date: '',
-          province: '',
-          city: '',
-          address: '',
-          zip: ''
-        };
-        this.isadduser = false;
-      },
-      submitForm(){
-        this.isadduser = false;
-        axios.post('http://localhost:8081/api/addauthor',this.form)
-          .then((response) => {
-            // 请求成功处理
-            console.log(response.data);
-            this.form = {
-              name: '',
-              date: '',
-              province: '',
-              city: '',
-              address: '',
-              zip: ''
-            };
-            this.$message.success('新增成功');
-          })
-          .catch((error) => {
-            // 请求失败处理
-            console.error(error);
-          });
-      },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClick(row) {
+      console.log(row);
+    },
+    pushHome(){
+      this.$router.push('/home');
+    },
+    docManage(){
+      this.$router.push('/docmanage');
+    },
+    yellowPanel(){
+      this.headerColor = '#b2b528';
+      this.mainColor = '#fff957';
+    },
+    bluePanel(){
+      this.headerColor = '#1E88E5';
+      this.mainColor = '#BBDEFB';
+    },
+    adduser(){
+      console.log('adduser');
+      this.isadduser = true;
+    },
+    searchuser(){
+      console.log('searchuser');
+    },
+    quitForm(){
+      this.form = {
+        name: '',
+        date: '',
+        province: '',
+        city: '',
+        address: '',
+        zip: ''
+      };
+      this.isadduser = false;
+    },
+    submitForm(){
+      this.isadduser = false;
+      axios.post('http://localhost:8081/api/addauthor', this.form)
+        .then((response) => {
+          // 请求成功处理
+          console.log(response.data);
+          this.form = {
+            name: '',
+            date: '',
+            province: '',
+            city: '',
+            address: '',
+            zip: ''
+          };
+          this.$message.success('新增成功');
+        })
+        .catch((error) => {
+          // 请求失败处理
+          console.error(error);
+        });
+    },
+    loadProvinces() {
+      AMap.plugin('AMap.CitySearch', () => {
+        const citySearch = new AMap.CitySearch();
+        citySearch.getLocalCity((status, result) => {
+          if (status === 'complete' && result.info === 'OK') {
+            this.provinces = result.provinceList.map(province => ({
+              value: province.name,
+              label: province.name
+            }));
+            console.log(this.provinces);
+          }
+        });
+      });
+    },
+    loadCities() {
+      if (!this.form.province) return;
+      AMap.plugin('AMap.DistrictSearch', () => {
+        const districtSearch = new AMap.DistrictSearch({
+          subdistrict: 1,
+          extensions: 'base'
+        });
+        districtSearch.search(this.form.province, (status, result) => {
+          if (status === 'complete' && result.info === 'OK') {
+            this.cities = result.districtList[0].districtList.map(city => ({
+              value: city.name,
+              label: city.name
+            }));
+          }
+        });
+      });
     }
   }
-  </script>
+};
+</script>
 
-  <style scoped>
-  .main-page {
-    position: fixed;
-    height: 100%;
-    width: 100%;
-    top: 0%;
-    left: 0%;
-    height: 100vh;
-  }
-  .upfield {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 10px;
-  }
-  .right-input {
-    width: 250px;
-    margin-left: auto;
-    margin-right: 10px;
-  }
-  .form {
-    height: 90%;
-  }
-  .container {
-    height: 100%;
-  }
-  
-  .content-container {
-    height: 100%;
-  }
-  
-  .header {
-    background-color: #1E88E5; /* 深蓝色 */
-    color: #fff;
-  }
-  
+<style scoped>
+.main-page {
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  top: 0%;
+  left: 0%;
+  height: 100vh;
+}
+.upfield {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+}
+.right-input {
+  width: 250px;
+  margin-left: auto;
+  margin-right: 10px;
+}
+.form {
+  height: 90%;
+}
+.container {
+  height: 100%;
+}
+
+.content-container {
+  height: 100%;
+}
+
+.header {
+  background-color: #1E88E5; /* 深蓝色 */
+  color: #fff;
+}
+
+.aside {
+  background-color: #fff; /* 白色 */
+  height: 100%;
+}
+
+.main {
+  background-color: #BBDEFB; /* 浅蓝色 */
+  height: 100%;
+}
+.full-width {
+  width: 100%;
+}
+.el-date-editor.el-input, .el-date-editor.el-input__inner {
+  width: 100%;
+}
+
+/* 媒体查询 */
+@media screen and (max-width: 800px) {
   .aside {
-    background-color: #fff; /* 白色 */
-    height: 100%;
+    display: none; /* 当屏幕宽度小于800px时，隐藏aside */
   }
-  
-  .main {
-    background-color: #BBDEFB; /* 浅蓝色 */
-    height: 100%;
-  }
-  .full-width {
-    width: 100%;
-  }
-  .el-date-editor.el-input, .el-date-editor.el-input__inner {
-    width: 100%;
-  }
-  
-  
-  /* 媒体查询 */
-  @media screen and (max-width: 800px) {
-    .aside {
-      display: none; /* 当屏幕宽度小于800px时，隐藏aside */
-    }
-  }
-  </style>
-  
+}
+</style>
